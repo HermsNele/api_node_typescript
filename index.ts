4630ea935564f8dd259728322776da224814f592
HermsNele <nelehermen@hotmail.com>
2024-08-16 17:44:15 +0100

    indexupdated

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import { Knex } from './src/server/database/knex';
 import { server } from './src/server/Server';
 import 'dotenv/config';
 console.log('I restarted at:', Date.now());
+const port = process.env.PORT || 4000;
 const startServer = () => {
-  server.listen(process.env.PORT || 4000, () => {
-    console.log(`App rodando na porta ${process.env.PORT}`);
+  server.listen(port, () => {
+    console.log(`App rodando na porta ${port}`);
   });
 };
+
 if (process.env.IS_LOCALHOST !== 'true') {
   console.log(process.env.IS_LOCALHOST);
   Knex.migrate

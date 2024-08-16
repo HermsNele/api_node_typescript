f646cf782ab1cdd5e9929c4fb8b4ce548ee6ab2f
HermsNele <nelehermen@hotmail.com>
2024-08-16 13:36:29 +0100

    update

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Knex } from './src/server/database/knex';
 import { server } from './src/server/Server';
 console.log('I restarted at:', Date.now());
 const startServer = () => {
-  server.listen(Bun.env['PORT'] || 3333, () => {
+  server.listen(process.env['PORT'] || 3333, () => {
     console.log(`App rodando na porta ${process.env['PORT'] || 3333}`);
   });
 };

ccb17aa4935c9cd429e96ac96c5230aca7296e84
HermsNele <nelehermen@hotmail.com>
2024-08-16 15:20:24 +0100

    updateindex

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ import { Knex } from './src/server/database/knex';
 import { server } from './src/server/Server';
 console.log('I restarted at:', Date.now());
 const startServer = () => {
-  server.listen(process.env['PORT'] || 3333, () => {
-    console.log(`App rodando na porta ${process.env['PORT'] || 3333}`);
+  server.listen(process.env.PORT || 4000, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 4000}`);
   });
 };
 if (process.env.IS_LOCALHOST !== 'true') {
